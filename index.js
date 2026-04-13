ce66cb38e6cc806f68cc400021344bcb1e868bb9
Zaid Bin Rashid <zaidbinrashid85@gmail.com>
2026-04-13 20:40:28 +0530

    added file and did  linked together file with each other (using MVP file structure)

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 import express from "express"
+import userRoutes from './Routes/userRoutes.js';
 
 const app = express()
 const port  = 4001;
 
-app.get('/', (req, res) =>{
-    res.send('Hello, World!')
-})
+// Global Middleware.
+app.use(express.json())
+app.use('/', userRoutes);
+
+
 
 app.listen(port, () => {
     console.log(`Server running on port: ${port}`);
