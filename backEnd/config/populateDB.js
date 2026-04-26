668fac6806239515c6d7eec86cb835d049fefc0c
ZaidBinRashid <zaidbinrashid85@gmail.com>
2026-04-26 21:19:45 +0530

    added products table to db

--- backEnd/config/populateDB.js
+++ backEnd/config/populateDB.js
@@ -1,11 +1,15 @@
-import { Client } from  "pg";
+import { Client } from "pg";
 import dotenv from "dotenv";
 dotenv.config({ path: "../.env" });
 
-
-
 const SQL = `
-
+    CREATE TABLE IF NOT EXISTS products  (
+      id SERIAL PRIMARY KEY,
+      title VARCHAR(200) NOT NULL,
+      quantity NUMERIC(10, 2) NOT NULL,
+      in_stock BOOLEAN DEFAULT TRUE,
+      created_at TIMESTAMP DEFAULT  CURRENT_TIMESTAMP
+);
 `
 
 async function main() {
--- backEnd/index.js
+++ backEnd/index.js
@@ -1,5 +1,5 @@
 import express from "express"
-import userRoutes from './routes/userRoutes.js';
+import productRoutes from './routes/productRoutes.js';
 import 'dotenv/config'
 import cors from 'cors'
 
@@ -10,7 +10,7 @@ const port  = process.env.PORT;
 app.use(express.json()) 
 app.use(cors())
 
-app.use('/api/user', userRoutes);
+app.use('/api/product', productRoutes);
 
 
 
