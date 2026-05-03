673e2ec18c1ea5d5c216c00663233fa9ec766af1
ZaidBinRashid <zaidbinrashid85@gmail.com>
2026-05-03 21:14:51 +0530

    added user signup func

--- backEnd/controllers/userController.js
+++ backEnd/controllers/userController.js
@@ -1,9 +1,10 @@
 import pool from "../config/pool.js";
 import { validationResult } from "express-validator";
-import bcrypt from 'bcrypt'
+import bcrypt from "bcrypt";
 
-export const signUp = (req, res) => {
+export const signUp = async (req, res) => {
   const { userName, email, password } = req.body;
+
   try {
     // Check validation errors
     const errors = validationResult(req);
@@ -11,8 +12,31 @@ export const signUp = (req, res) => {
       return res.status(400).json({ errors: errors.array() });
     }
 
+    const userExits = await pool.query(
+      ` SELECT * FROM users WHERE email = $1
+        `,
+      [email],
+    );
 
+    if (userExits.rows.length > 0) {
+      return res.status(400).json({ message: "User already exists" });
+    }
 
-    res.status(201).json({ message: "User created successfully" });
-  } catch (error) {}
+    const hashedPassword = bcrypt.hash(password, 10);
+
+    const result = await pool.query(
+      `INSERT INTO users (userName, email, password)
+         VALUES ($1, $2, $3)
+         RETURNING id userName, email`,
+      [userName, email, hashedPassword],
+    );
+
+    res.status(201).json({
+      message: "User created successfully",
+      user: result.rows[0],
+    });
+  } catch (err) {
+    console.error("Signup Error:", err);
+    res.status(500).json({ message: "Server error" });
+  }
 };
